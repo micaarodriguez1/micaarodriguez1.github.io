c59eb4096c83fa5fc0820f210d2b2ab396853159
micaarodriguez1 <micaelaro29@gmail.com>
2023-02-17 22:55:04 -0300

    terminado

--- scrips/generar_pdf.js
+++ scrips/generar_pdf.js
@@ -1,9 +1,22 @@
+window.jsPDF = window.jspdf.jsPDF;
+window.html2canvas = html2canvas;
 
-const doc = new jsPDF();
+
 const btn = document.querySelector('#boton');
 function generarPDF()
 {
-    alert('entro')
-    doc.text("Hello world!", 1, 1);
-doc.save("two-by-four.pdf");
+    html2canvas(document.querySelector("#capture"), {
+        allowTaint: true,
+        useCORS: true,
+        scale: 1,
+    }).then(canvas => {
+        const doc = new jsPDF('p', 'pt', 'a4');
+        var width = doc.internal.pageSize.getWidth();
+        var height = doc.internal.pageSize.getHeight();
+        let img = canvas.toDataURL("image/png");
+        doc.setFont('Arial');
+        doc.setFontSize(11);
+        doc.addImage(img, 'PNG', 0, 0, width, height);
+        doc.save('MICA.pdf');
+    });
 }
